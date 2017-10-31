50526ce6a5017d1f915e64a01ed810dca6cc23e6
sunwentao <sunwentao@primedu.cn>
2017-10-31 14:49:57 +0800

    练习项目3结束

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice01DrawTextView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice01DrawTextView.java
@@ -31,6 +31,7 @@ public class Practice01DrawTextView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.drawText("Hen Coder",100,200,paint);
         // 使用 drawText() 来绘制文字
         // 文字坐标： (50, 100)
     }
